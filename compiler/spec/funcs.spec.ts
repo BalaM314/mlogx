47266e261e2101665bf4a9b709dfb179248e13b0
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-06-14 10:11:46 +0530

    start adding unit test

--- compiler/spec/funcs.spec.ts
+++ compiler/spec/funcs.spec.ts
@@ -1,3 +1,5 @@
+import { isArgOfType } from "../funcs.js";
+import { cleanLine } from "../funcs.js";
 import { isGenericArg, typeofArg } from "../funcs.js";
 import { GenericArgType } from "../types.js";
 
@@ -13,16 +15,63 @@ describe("isGenericArg", () => {
 
 describe("typeofArg", () => {
 	it("should determine the type of an arg", () => {
-		const args = [
+		const args: [arg:string, expectedType:string][] = [
 			["@unit", GenericArgType.unit],
 			["@thisx", GenericArgType.number],
 			["@this", GenericArgType.building],
 			["greaterThanEq", GenericArgType.operandTest],
-			["-50.2", GenericArgType.number]
+			["-50.2", GenericArgType.number],
+			[`"amogus"`, GenericArgType.string]
 		];
 		for(let [arg, expectedOutput] of args){
 			expect(typeofArg(arg)).toBe(expectedOutput);
 		}
 	});
 });
+
+describe("isArgOfType", () => {
+	it("should determine if an arg is of specified type", () => {
+		const correctTypes: [arg:string, expectedType:GenericArgType][] = [
+			["@unit", GenericArgType.unit],
+			["@thisx", GenericArgType.number],
+			["@this", GenericArgType.building],
+			["greaterThanEq", GenericArgType.operandTest],
+			["-50.2", GenericArgType.number],
+			[`"amogus"`, GenericArgType.string],
+			["sussyFlarogus", GenericArgType.unit]
+		];
+		const wrongTypes: [arg:string, expectedType:GenericArgType][] = [
+			["@unit", GenericArgType.building],
+			["@thisx", GenericArgType.operandTest],
+			["@this", GenericArgType.string],
+			["greaterThanEq", GenericArgType.buildingGroup],
+			["-50.2", GenericArgType.unit],
+			[`"amogus"`, GenericArgType.number],
+		];
+		for(let [arg, expectedType] of correctTypes){
+			expect(isArgOfType(arg, expectedType)).toBe(true);
+		}
+		for(let [arg, unexpectedType] of correctTypes){
+			expect(isArgOfType(arg, unexpectedType)).toBe(false);
+		}
+	});
+});
+
+describe("cleanLine", () => {
+	it("should not modify lines that are already clean", () => {
+		expect(cleanLine("set x 5")).toBe("set x 5");
+		expect(cleanLine("ulocate building core true outX outY found building")).toBe("ulocate building core true outX outY found building");
+	});
+	it("should remove trailing and leading spaces and tabs", () => {
+		expect(cleanLine("    set x 5")).toBe("set x 5");
+		expect(cleanLine("set x 5  ")).toBe("set x 5");
+		expect(cleanLine("\tset x 5")).toBe("set x 5");
+		expect(cleanLine(" \tset x 5\t ")).toBe("set x 5");
+	});
+	it("should remove single line comments", () => {});
+	it("should remove multiline comments", () => {});
+	it("should not mess up comments inside of strings", () => {});
+	it("should do everything above at once", () => {});
+})
+
 
