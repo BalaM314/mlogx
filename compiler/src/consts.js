bd8e9f690cec3f85c16764e9a7b9330fae9f929d
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-04-23 22:40:23 +0530

    Update building list

--- compiler/src/consts.js
+++ compiler/src/consts.js
@@ -3,27 +3,6 @@ export const compilerMark = [
     `print "github.com/BalaM314/mlogx/"`
 ];
 export const bugReportUrl = `https://github.com/BalaM314/mlogx/issues/new`;
-export const buildingInternalNames = [
-    "press", "smelter", "crucible", "kiln", "compressor",
-    "weaver", "mixer", "melter", "separator", "disassembler",
-    "pulverizer", "centrifuge", "incinerator", "wall",
-    "door", "thruster", "mender", "projector", "dome",
-    "mine", "conveyor", "junction", "sorter", "router",
-    "distributor", "gate", "driver", "duct", "bridge",
-    "pump", "conduit", "container", "tank", "node", "tower",
-    "diode", "battery", "generator", "panel", "reactor",
-    "drill", "extractor", "cultivator", "shard", "foundation",
-    "nucleus", "vault", "unloader", "duo", "scatter",
-    "scorch", "hail", "wave", "lancer", "arc", "parallax",
-    "swarmer", "salvo", "segment", "tsunami", "fuse", "ripple",
-    "cyclone", "foreshadow", "spectre", "meltdown", "center",
-    "factory", "reconstructor", "point", "turret",
-    "deconstructor", "constructor", "loader", "source",
-    "void", "illuminator", "air", "ground", "pad",
-    "accelerator", "message", "switch", "processor",
-    "cell", "bank", "display"
-];
-export const buildingNameRegex = new RegExp(`^(${buildingInternalNames.map(el => `(${el})`).join("|")})[\\d]+$`);
 export const requiredVarCode = {
     "cookie": [[
             `op mul cookie @thisx @maph`,
@@ -80,6 +59,7 @@ export const shortOperandMappings = {
 export const MindustryContent = {
     items: ["copper", "lead", "metaglass", "graphite", "sand", "coal", "titanium", "thorium", "scrap", "silicon", "plastanium", "phase-fabric", "surge-alloy", "spore-pod", "blast-compound", "pyratite", "beryllium", "fissile-matter", "dormant-cyst", "tungsten", "carbide", "oxide"],
     buildings: ["graphite-press", "multi-press", "silicon-smelter", "silicon-crucible", "kiln", "plastanium-compressor", "phase-weaver", "cryofluid-mixer", "pyratite-mixer", "blast-mixer", "melter", "separator", "disassembler", "spore-press", "pulverizer", "coal-centrifuge", "incinerator", "copper-wall", "copper-wall-large", "titanium-wall", "titanium-wall-large", "plastanium-wall", "plastanium-wall-large", "thorium-wall", "thorium-wall-large", "phase-wall", "phase-wall-large", "surge-wall", "surge-wall-large", "door", "door-large", "scrap-wall", "scrap-wall-large", "scrap-wall-huge", "scrap-wall-gigantic", "mender", "mend-projector", "overdrive-projector", "overdrive-dome", "force-projector", "shock-mine", "conveyor", "titanium-conveyor", "plastanium-conveyor", "armored-conveyor", "junction", "bridge-conveyor", "phase-conveyor", "sorter", "inverted-sorter", "router", "distributor", "overflow-gate", "underflow-gate", "mass-driver", "duct", "duct-router", "duct-bridge", "mechanical-pump", "rotary-pump", "conduit", "pulse-conduit", "plated-conduit", "liquid-router", "liquid-tank", "liquid-junction", "bridge-conduit", "phase-conduit", "power-node", "power-node-large", "surge-tower", "diode", "battery", "battery-large", "combustion-generator", "thermal-generator", "steam-generator", "differential-generator", "rtg-generator", "solar-panel", "solar-panel-large", "thorium-reactor", "impact-reactor", "mechanical-drill", "pneumatic-drill", "laser-drill", "blast-drill", "water-extractor", "cultivator", "oil-extractor", "core-shard", "core-foundation", "core-nucleus", "vault", "container", "unloader", "duo", "scatter", "scorch", "hail", "wave", "lancer", "arc", "parallax", "swarmer", "salvo", "segment", "tsunami", "fuse", "ripple", "cyclone", "foreshadow", "spectre", "meltdown", "command-center", "ground-factory", "air-factory", "naval-factory", "additive-reconstructor", "multiplicative-reconstructor", "exponential-reconstructor", "tetrative-reconstructor", "repair-point", "repair-turret", "payload-conveyor", "payload-router", "payload-propulsion-tower", "power-source", "power-void", "item-source", "item-void", "liquid-source", "liquid-void", "payload-void", "payload-source", "illuminator", "launch-pad", "interplanetary-accelerator", "message", "switch", "micro-processor", "logic-processor", "hyper-processor", "memory-cell", "memory-bank", "logic-display", "large-logic-display", "liquid-container", "deconstructor", "constructor", "thruster", "large-constructor", "payload-loader", "payload-unloader", "silicon-arc-furnace", "cliff-crusher", "plasma-bore", "reinforced-liquid-junction", "breach", "core-bastion", "turbine-condenser", "beam-node", "beam-tower", "build-tower", "impact-drill", "carbide-crucible", "surge-conveyor", "duct-unloader", "surge-router", "reinforced-conduit", "reinforced-liquid-router", "reinforced-liquid-container", "reinforced-liquid-tank", "reinforced-bridge-conduit", "core-citadel", "core-acropolis", "heat-reactor", "impulse-pump", "reinforced-pump", "electrolyzer", "oxidation-chamber", "surge-smelter", "surge-crucible", "overflow-duct", "large-plasma-bore", "cyanogen-synthesizer", "slag-centrifuge", "electric-heater", "slag-incinerator", "phase-synthesizer", "sublimate", "reinforced-container", "reinforced-vault", "atmospheric-concentrator", "unit-cargo-loader", "unit-cargo-unload-point", "chemical-combustion-chamber", "pyrolysis-generator", "regen-projector", "titan", "small-deconstructor", "vent-condenser", "phase-heater", "heat-redirector", "tungsten-wall", "tungsten-wall-large", "tank-assembler", "beryllium-wall", "beryllium-wall-large", "eruption-drill", "ship-assembler", "mech-assembler", "shield-projector", "beam-link", "world-processor", "reinforced-payload-conveyor", "reinforced-payload-router", "disperse", "large-shield-projector", "payload-mass-driver", "world-cell", "carbide-wall", "carbide-wall-large", "tank-fabricator", "mech-fabricator", "ship-fabricator", "reinforced-surge-wall", "radar", "blast-door", "canvas", "armored-duct", "shield-breaker", "unit-repair-tower", "diffuse", "prime-refabricator", "basic-assembler-module", "reinforced-surge-wall-large", "tank-refabricator", "mech-refabricator", "ship-refabricator", "slag-heater", "afflict", "shielded-wall", "lustre", "scathe", "smite", "underflow-duct", "malign", "shockwave-tower", "heat-source", "flux-reactor", "neoplasia-reactor"],
+    buildingInternalNames: [],
     fluids: ["water", "slag", "oil", "cryofluid", "neoplasm", "hydrogen", "ozone", "cyanogen", "gallium", "nitrogen", "arkycite"],
     units: ["dagger", "mace", "fortress", "scepter", "reign", "nova", "pulsar", "quasar", "vela", "corvus", "crawler", "atrax", "spiroct", "arkyid", "toxopid", "flare", "horizon", "zenith", "antumbra", "eclipse", "mono", "poly", "mega", "quad", "oct", "risso", "minke", "bryde", "sei", "omura", "retusa", "oxynoe", "cyerce", "aegires", "navanax", "alpha", "beta", "gamma", "stell", "locus", "precept", "vanquish", "conquer", "merui", "cleroi", "anthicus", "tecta", "collaris", "elude", "avert", "obviate", "quell", "disrupt", "evoke", "incite", "emanate"],
     senseables: ["totalItems", "firstItem", "totalLiquids", "totalPower", "itemCapacity", "liquidCapacity", "powerCapacity", "powerNetStored", "powerNetCapacity", "powerNetIn", "powerNetOut", "ammo", "ammoCapacity", "health", "maxHealth", "heat", "efficiency", "progress", "timescale", "rotation", "x", "y", "shootX", "shootY", "size", "dead", "range", "shooting", "boosting", "mineX", "mineY", "mining", "speed", "team", "type", "flag", "controlled", "controller", "name", "payloadCount", "payloadType", "enabled", "shoot", "shootp", "config", "color"],
@@ -90,3 +70,5 @@ MindustryContent.senseables.push(...MindustryContent.items);
 MindustryContent.senseables.push(...MindustryContent.fluids);
 MindustryContent.settables.push(...MindustryContent.items);
 MindustryContent.settables.push(...MindustryContent.fluids);
+MindustryContent.buildingInternalNames = [...new Set(MindustryContent.buildings.map(build => build.split("-").reverse().find(s => !["large", "huge", "gigantic"].includes(s))))];
+export const buildingNameRegex = new RegExp(`^(${MindustryContent.buildingInternalNames.map(el => `(${el})`).join("|")})[\\d]+$`);
