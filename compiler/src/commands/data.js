96ce7d8ef5d9272208b2871b7cfad0eb7ee2fe5b
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-09-18 14:13:46 +0530

    Porting for setrule

--- compiler/src/commands/data.js
+++ compiler/src/commands/data.js
@@ -593,6 +593,24 @@ export const commands = processCommands({
             args: "rule:ruleAny value:number xOrTeam:any y:number width:number height:number",
             description: "Default setrule signature. Bad.",
             isWorldProc: true,
+            port([rule, value, xOrTeam, y, w, h], mode) {
+                if (rule == "mapArea" && mode >= PortingMode.removeZeroes) {
+                    return `setrule mapArea ${xOrTeam} ${y} ${w} ${h}`;
+                }
+                else if (GenericArgs.get("ruleTeam").validator.includes(rule) && mode >= PortingMode.removeZeroes) {
+                    if (mode >= PortingMode.modernSyntax) {
+                        return `setrule ${rule} ${xOrTeam} ${value}`;
+                    }
+                    else {
+                        return `setrule ${rule} ${value} ${xOrTeam}`;
+                    }
+                }
+                else if (GenericArgs.get("ruleNormal").validator.includes(rule) && mode >= PortingMode.removeZeroes) {
+                    return `setrule ${rule} ${value}`;
+                }
+                else
+                    impossible();
+            },
         },
     ],
     message: [
